520e964017eb4dba75bc66d13c48b7b6ff9ae009
hiennguyendang <hien.nguyendang@hcmut.edu.vn>
2025-01-27 01:03:11 +0700

    new update in sites

--- script.js
+++ script.js
@@ -21,3 +21,56 @@ window.onclick = function (event) {
         closeModal();
     }
 };
+
+window.onload = function() {
+    // Chọn nút đầu tiên làm mặc định
+    document.querySelector('.button').classList.add('active');
+
+    // Hiển thị nội dung mặc định
+    changeContent1('content1');
+    changeContent2('content1');
+};
+
+function changeContent1(contentId) {
+    // Lấy phần tử chứa nội dung
+    const contentElement = document.getElementById('content-1');
+    // Thay đổi nội dung của phần tử dựa trên id truyền vào
+    if (contentId === 'content1') {
+        const buttonContent = document.querySelector('.button-content-1').innerHTML;
+        contentElement.innerHTML = buttonContent;
+    } else if (contentId === 'content2') {
+        const buttonContent = document.querySelector('.button-content-2').innerHTML;
+        contentElement.innerHTML = buttonContent;
+    } else if (contentId === 'content3') {
+        const buttonContent = document.querySelector('.button-content-3').innerHTML;
+        contentElement.innerHTML = buttonContent;
+    } else if (contentId === 'content4') {
+        const buttonContent = document.querySelector('.button-content-4').innerHTML;
+        contentElement.innerHTML = buttonContent;
+    } else {
+        const buttonContent = document.querySelector('.button-content-1').innerHTML;
+        contentElement.innerHTML = buttonContent;
+    }
+}
+
+function changeContent2(contentId) {
+    // Lấy phần tử chứa nội dung
+    const contentElement = document.getElementById('content-2');
+    // Thay đổi nội dung của phần tử dựa trên id truyền vào
+    if (contentId === 'content1') {
+        const buttonContent = document.querySelector('.button-content-1').innerHTML;
+        contentElement.innerHTML = buttonContent;
+    } else if (contentId === 'content2') {
+        const buttonContent = document.querySelector('.button-content-2').innerHTML;
+        contentElement.innerHTML = buttonContent;
+    } else if (contentId === 'content3') {
+        const buttonContent = document.querySelector('.button-content-3').innerHTML;
+        contentElement.innerHTML = buttonContent;
+    } else if (contentId === 'content4') {
+        const buttonContent = document.querySelector('.button-content-4').innerHTML;
+        contentElement.innerHTML = buttonContent;
+    } else {
+        const buttonContent = document.querySelector('.button-content-1').innerHTML;
+        contentElement.innerHTML = buttonContent;
+    }
+}
